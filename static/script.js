8160f28f6c336c57d2e6def05c88730ae0a263c8
Rodhv <103824465+Rodhv@users.noreply.github.com>
2024-03-07 20:58:33 +0530

    Update script.js
    tabs

--- static/script.js
+++ static/script.js
@@ -129,3 +129,20 @@ function redirectToVisualizeData() {
 }
 // Load stocks when window is loaded
 window.onload = loadStocks;
+
+// year and month tabs
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+    // By default, display the "Yearly" tab
+    document.getElementById("defaultOpen").click();
