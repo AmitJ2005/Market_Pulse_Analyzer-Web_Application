6de57d7424d37790fd7b271a94774af65b9db068
Amit Jaiswal <amitjaisawal0123@gmail.com>
2024-05-25 21:16:37 +0530

    remove unused line of code

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 import pandas as pd
 import yfinance as yf
 from flask import Flask, render_template, request, jsonify, send_from_directory
-from yahooquery import search
 import json
 
 app = Flask(__name__)
 df = pd.DataFrame()
-selected_stock = ''
 stock_symbol = ''
-result_df_same_month = None
-result_df_yearly = None
-result_info = None
-year_income = None
 stock_ticker = None
+result_info = None
 stock_information = None
 
 def stock_info():
@@ -33,9 +28,7 @@ def stock_info():
 def fetch_info(stock_symbol):
     global stock_ticker
     try:
-        # Create a Ticker object for the stock
         stock_ticker = yf.Ticker(stock_symbol)
-        # Get company information
         company_info = stock_ticker.info
         general_info = {
             "Company Name": company_info.get("longName", ""),
@@ -55,7 +48,6 @@ def fetch_info(stock_symbol):
             "Total Share in Company": company_info.get("sharesOutstanding",""),
             "enterpriseValue": company_info.get("enterpriseValue",""),
         }
-        # Fetch company officers data
         company_officers = []
         for officer in company_info.get("companyOfficers", []):
             officer_info = {
@@ -65,16 +57,12 @@ def fetch_info(stock_symbol):
             }
             company_officers.append(officer_info)
 
-        # Add company officers data to general_info
         general_info["companyOfficers"] = company_officers
         return general_info
-
     except Exception as e:
         print(f"Error fetching stock information for {stock_symbol}: {e}")
         return None
 
-
-# Function to fetch historical data using yfinance
 def fetch_historical_data(stock_symbol):
     try:
         ticker = yf.Ticker(stock_symbol)
@@ -84,55 +72,41 @@ def fetch_historical_data(stock_symbol):
         print(f"Error fetching historical data for {stock_symbol}: {e}")
         return None
 
-
-# Function to preprocess the data
 def preprocess_data(data):
-    data = data.drop(columns=["Adj Close"])
+    if "Adj Close" in data.columns:
+        data = data.drop(columns=["Adj Close"])
     data = data.round(2)
     return data
 
-
-# Function to handle fetching and preprocessing data for selected stock
 def handle_selected_stock(selected_stock):
     global df
     global stock_symbol
     global result_info
-    global year_income
     global stock_information
 
-    # Fetch stock symbol based on the selected stock
     try:
         with open('stock_names.json', 'r') as f:
             stock_symbols = json.load(f)
-
-        # Get the stock symbol for the selected stock
         stock_symbol = stock_symbols.get(selected_stock)
-        print(stock_symbol)
 
         if stock_symbol is None:
             print(f"No symbol found for stock: {selected_stock}")
-            return
+            return None, None
 
-        print(f"Stock symbol for {selected_stock}: {stock_symbol}")
         stock_information = stock_info()
-        # Fetch company information
         result_info = fetch_info(stock_symbol)
-        # Fetch historical data
         df = fetch_historical_data(stock_symbol)
         if df is not None:
-            # Preprocess the data
             df = preprocess_data(df)
         else:
             print("Failed to fetch or preprocess data.")
-        return result_info
+        return result_info, stock_information
     except Exception as e:
         print(f"Error fetching stock symbol for {selected_stock}: {e}")
+        return None, None
 
-
-# Route to serve the HTML page
 @app.route('/')
 def index():
-    print("Rendering index.html")
     return render_template('index.html')
 
 @app.route('/about')
@@ -149,22 +123,21 @@ def login():
 
 @app.route('/stock_names.json')
 def get_stock_names():
-    # Logic to read and return stock_names.json
     return send_from_directory(app.root_path, 'stock_names.json')
 
-
-# Route to handle the selected stock and fetch historical data
 @app.route('/submit_selected_stock', methods=['POST'])
 def submit_selected_stock():
-    global selected_stock
+    global result_info
+    global stock_information
+    
     data = request.get_json()
     selected_stock = data.get('selectedStock', '')
-    # Handle selected stock
-    handle_selected_stock(selected_stock)
-    return jsonify({'message': 'Selected stock received successfully'})
+    result_info, stock_information = handle_selected_stock(selected_stock)
+    if result_info and stock_information:
+        return jsonify({'message': 'Selected stock received successfully'})
+    else:
+        return jsonify({'message': 'Failed to process selected stock'}), 500
 
-
-# Visualization route to generate and display plots
 @app.route('/visualize_data')
 def visualize_data():
     global df
@@ -173,7 +146,7 @@ def visualize_data():
 
     # First visualization
     if df is None or df.empty:
-        error_message = "aaj mood nhi hai thoda scroll kr leta hu"
+        error_message = "No data available for visualization"
         print(error_message)
         return jsonify({'error': error_message}), 400
 
@@ -237,8 +210,8 @@ def visualize_data():
     result_df_yearly = result_df_yearly.round(2)
 
     return render_template('result.html', labels=json.dumps(labels), data=json.dumps(data),
-                           data_monthly=result_df.to_dict('records'), data_yearly=result_df_yearly.to_dict('records')
-                            , result_info=result_info, stock_information=stock_information)
+                           data_monthly=result_df.to_dict('records'), data_yearly=result_df_yearly.to_dict('records'),
+                           result_info=result_info, stock_information=stock_information)
 
 if __name__ == '__main__':
     app.run(debug=True)
